ac3e0424668982f7daeb97c50b30a14016052236
admin <shenno.w@gmail.com>
2015-10-31 22:44:04 +0100

    Init receiverproject

--- generator/src/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by admin on 31/10/2015.
- */
-public class Test {
-}
--- src/be/kdg/se3/exam/receiver/Test.java
+++ src/be/kdg/se3/exam/receiver/Test.java
@@ -1,6 +1,7 @@
+package be.kdg.se3.exam.receiver;
+
 /**
  * Created by admin on 31/10/2015.
  */
-
 public class Test {
 }
