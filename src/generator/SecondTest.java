3ff42804953edd1db816b75d5c4199823e653296
admin <shenno.w@gmail.com>
2015-10-31 22:00:41 +0100

    Initpush

--- src/generator/SecondTest.java
+++
@@ -1,7 +0,0 @@
-package generator;
-
-/**
- * Created by admin on 31/10/2015.
- */
-public class SecondTest {
-}
--- src/receiver/Test.java
+++
@@ -1,7 +0,0 @@
-package receiver;
-
-/**
- * Created by admin on 31/10/2015.
- */
-public class Test {
-}
